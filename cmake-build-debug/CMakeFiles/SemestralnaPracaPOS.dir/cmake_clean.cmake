0c6630e0d2e47a854a2b27b787d3181a6528ce20
VladimirKuderavy <80854449+VladimirKuderavy@users.noreply.github.com>
2022-01-03 22:59:19 +0100

    Prvy commit

---
+++ cmake-build-debug/CMakeFiles/SemestralnaPracaPOS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SemestralnaPracaPOS.dir/main.cpp.o"
+  "SemestralnaPracaPOS"
+  "SemestralnaPracaPOS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SemestralnaPracaPOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
